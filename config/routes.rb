3c66c8220ff61373c6c429934260bde7fc55ebb8
Usman Kabir <usman.kabir.k@gmail.com>
2020-03-09 19:51:50 +0500

    Article can be added updated and deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/pages/home', to: 'pages#home'
-  get '/pages/about', to: 'pages#about'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+
+  resources :articles
+
 end
